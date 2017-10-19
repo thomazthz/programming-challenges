348bf5813fbb2155ae882e3ec1f576132a74cab9
Thomaz Soares <thomazthz0@gmail.com>
2017-10-19 14:41:58 -0200

    Added python solution to the URI problem 2670 - Maquina de Cafe (Maratona de Programação da SBC 2017)

---
+++ URI/Beginner/2670_maquina-de-cafe.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+# Maratona de Programção da SBC, ACM ICPC 2017
+# https://www.urionlinejudge.com.br/judge/pt/problems/view/2670
+
+a1 = int(input())
+a2 = int(input())
+a3 = int(input())
+
+print(min((a2 * 2 + a3 * 4, a1 * 2 + a3 * 2, a1 * 4 + a2 * 2)))
